68af94ef90c38b215bcf0ae212f672912ddfb444
Itay Aviani <itay068@gmail.com>
2024-10-28 07:00:00 +0200

    thankyoupage

--- client/src/pages/courses/thankYouCourse/ThankYouCourse.jsx
+++ client/src/pages/courses/thankYouCourse/ThankYouCourse.jsx
@@ -4,12 +4,14 @@ import './thankYouCourse.css';
 export default function ThankYou() {
   return (
     <div className="thank-you-container-course">
-      <h1>תודה על הרכישה!</h1>
-      <p>הרכישה בוצעה בהצלחה, מייל עם פרטי הרכישה נשלח אליך.</p>
-      <p>אם יש לך שאלות נוספות, ניתן לפנות לשירות הלקוחות שלנו.</p>
-      <button onClick={() => window.location.href = '/courses'} className="back-button-course">
-        חזרה לקורסים
-      </button>
+      <div className="thank-you-box">
+        <h1>!תודה על הרכישה</h1>
+        <p>.הרכישה בוצעה בהצלחה, מייל עם פרטי הרכישה נשלח אליך</p>
+        <p>.אם יש לך שאלות נוספות, ניתן לפנות אלינו בדף צור קשר</p>
+        <button onClick={() => window.location.href = '/courses'} className="back-button-course">
+          חזרה לקורסים
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/pages/workshops/ThankYouWorkshop/ThankYouWorkshop.jsx
+++ client/src/pages/workshops/ThankYouWorkshop/ThankYouWorkshop.jsx
@@ -4,9 +4,9 @@ import './thankYouWorkshop.css';
 export default function ThankYouWorkshop() {
   return (
     <div className="thank-you-container-workshop">
-      <h1>תודה על הרכישה!</h1>
-      <p>הרכישה בוצעה בהצלחה, מייל עם פרטי הרכישה נשלח אליך.</p>
-      <p>אם יש לך שאלות נוספות, ניתן לפנות לשירות הלקוחות שלנו.</p>
+      <h1>!תודה על הרכישה</h1>
+      <p>.הרכישה בוצעה בהצלחה, מייל עם פרטי הרכישה נשלח אליך</p>
+      <p>.אם יש לך שאלות נוספות, ניתן לפנות אלינו בדף צור קשר</p>
       <button onClick={() => window.location.href = '/workshops'} className="back-button-workshop">
         חזרה לסדנאות
       </button>
